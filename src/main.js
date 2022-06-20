100a164b0e638e28645ccd7f562c5671a0749c59
leomw <bzishvili57@gmail.com>
2022-06-20 22:52:00 +0400

    install fontAwesomeIcons and vue-bootstrap

--- src/main.js
+++ src/main.js
@@ -2,5 +2,45 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import {library} from "@fortawesome/fontawesome-svg-core";
+import {
+    faTrash,
+    faAdd,
+    faCircleMinus,
+    faSquareCheck,
+    faCircleDot,
+    faSquareCaretDown,
+    faAlignLeft,
+    faCopy,
+    faXmark,
+    faClipboardCheck,
+    faImage,
+    faCloudArrowUp,
+}
+    from "@fortawesome/free-solid-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome"
+library.add(
+    faTrash,
+    faAdd,
+    faCircleMinus,
+    faSquareCheck,
+    faCircleDot,
+    faSquareCaretDown,
+    faAlignLeft,
+    faCopy,
+    faXmark,
+    faClipboardCheck,
+    faImage,
+    faCloudArrowUp,
+)
 
-createApp(App).use(store).use(router).mount('#app')
+const app = createApp(App)
+    .use(store)
+    .use(router)
+    .component("font-awesome-icon",FontAwesomeIcon)
+
+import BootstrapVue3 from "bootstrap-vue-3";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue-3/dist/bootstrap-vue-3.css";
+app.use(BootstrapVue3)
+app.mount('#app')
