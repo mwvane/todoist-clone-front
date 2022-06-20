2707a8b9d750dc3d5756618854cb0f3e20e99857
vladimer <gshatakishvili@gmail.com>
2022-06-21 00:22:18 +0400

    Add rules to eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,6 +12,9 @@ module.exports = {
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'vue/multi-word-component-names': 'off',
+    'no-unused-vars': 'off',
+    'vue/no-unused-vars': 'off',
   }
 }
