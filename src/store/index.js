484476cbd5fb697bed5f0850b7c308378b90dbfb
leomw <bzishvili57@gmail.com>
2022-06-21 17:48:53 +0400

    create IsSidebarOpen property in state

--- src/store/index.js
+++ src/store/index.js
@@ -2,12 +2,20 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    isAuthorised: true,
+    isSideBarOpened: true
   },
   getters: {
   },
   mutations: {
+    openCloseSidebar(state){
+      state.isSideBarOpened =!state.isSideBarOpened
+    }
   },
   actions: {
+    openCloseSidebar({commit}){
+      commit('openCloseSidebar')
+    }
   },
   modules: {
   }
