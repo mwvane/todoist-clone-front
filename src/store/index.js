19a850ad620e14424e805d4d06d47421de3ebd27
leomw <bzishvili57@gmail.com>
2022-06-22 00:40:42 +0400

    add isProjectCollapsed in state and actions

--- src/store/index.js
+++ src/store/index.js
@@ -3,18 +3,25 @@ import { createStore } from 'vuex'
 export default createStore({
   state: {
     isAuthorised: true,
-    isSideBarOpened: true
+    isSideBarOpened: true,
+    isProjectsCollapsed: false
   },
   getters: {
   },
   mutations: {
     openCloseSidebar(state){
       state.isSideBarOpened =!state.isSideBarOpened
+    },
+    collapseProjects(state){
+      state.isProjectsCollapsed = !state.isProjectsCollapsed
     }
   },
   actions: {
     openCloseSidebar({commit}){
       commit('openCloseSidebar')
+    },
+    collapseProjects({commit}){
+      commit('collapseProjects')
     }
   },
   modules: {
