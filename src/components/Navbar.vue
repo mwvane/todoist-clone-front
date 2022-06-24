c1f375e01b977bb7db6b8c46e0ca9a2fa2469e91
leomw <bzishvili57@gmail.com>
2022-06-24 22:55:18 +0400

    update navbar Functionality

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,9 +1,15 @@
 <template>
-  <div class="custom-navbar shadow position-relative">
+  <div class="custom-navbar position-relative">
     <div class="row">
       <div class="col-12">
         <font-awesome-icon  class="mx-2 fa-icons" icon="bars" @click="openSidebar"></font-awesome-icon>
         <font-awesome-icon  class="mx-3 fa-icons" icon="house-chimney"></font-awesome-icon>
+        <div class="actions">
+          <font-awesome-icon class="fa-icons mx-1" icon="plus"></font-awesome-icon>
+          <font-awesome-icon class="fa-icons mx-1" icon="circle-question"></font-awesome-icon>
+          <font-awesome-icon class="fa-icons mx-1" icon="bell"></font-awesome-icon>
+          <AvatarComponent></AvatarComponent>
+        </div>
         <slot></slot>
       </div>
     </div>
@@ -12,10 +18,10 @@
 
 <script>
 import {mapActions} from "vuex";
-
+import AvatarComponent from "@/components/AvatarComponent";
 export default {
   name: "Navbar",
-  components: {},
+  components: {AvatarComponent},
   methods:{
     ...mapActions(['openCloseSidebar']),
     openSidebar(){
@@ -27,12 +33,19 @@ export default {
 </script>
 
 <style scoped>
+.actions{
+  position: absolute;
+  right: 8px;
+  top:8px;
+  display: flex;
+}
 .custom-navbar{
   font-size: 20px;
   color: white;
-  background: #2c2b2b;
+  background: #282828;
   padding: 5px;
   overflow-x: hidden;
+  box-shadow: 1px 1px 2px 2px rgba(45,45,45, .6);
 }
 .fa-icons{
   padding: 6px;
@@ -43,6 +56,6 @@ export default {
 
 }
 .fa-icons:hover{
-  background-color: #5a5959;
+  background-color: #363636;
 }
 </style>
