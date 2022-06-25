fb4f9bfb13cc92bbb8597878e060f5a572a9cb1c
vladimer <gshatakishvili@gmail.com>
2022-06-25 23:30:05 +0400

    Update axiosClient.js

--- src/shared/axiosClient.js
+++ src/shared/axiosClient.js
@@ -15,4 +15,29 @@ axiosClient.interceptors.request.use(config => {
     return config;
 });
 
-export default axiosClient;
+function getResponseBody(status, data) {
+    return {
+        ok: status < 400,
+        status: status,
+        data: data
+    }
+}
+
+async function runRequest(method, ...params) {
+    try {
+        const res = await axiosClient[method](...params)
+        return getResponseBody(res.status, res.data)
+    } catch (res) {
+        return getResponseBody(res.response.status, res.response.data)
+    }
+}
+
+const handler = {
+    get: async (url, params) => runRequest('get', url, params),
+    post: async (url, data, config) => runRequest('post', url, data, config),
+    put: async (url, data, config) => runRequest('put', url, data, config),
+    delete: async (url, params) => runRequest('delete', url, params),
+    patch: async (url, params) => runRequest('patch', url, params)
+}
+
+export default handler;
